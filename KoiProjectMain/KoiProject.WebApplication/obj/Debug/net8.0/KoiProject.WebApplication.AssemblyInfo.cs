46629278782b49eaa1931b98407f82cb9d5d9b8a
NguyenDucThinh10 <thinhzz571@gmail.com>
2024-11-01 13:33:14 +0700

    Update .gitignore and clear cache

--- KoiProjectMain/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
+++ KoiProjectMain/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d07653c0e4027f3d8b6b0002161fa5e488d2ee9")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b54b5a2a5812908f5f39ce622a4dc2cd77c8e4")]
+>>>>>>> b4cf550 (Cover)
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
