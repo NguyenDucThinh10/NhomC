fcb6d63a9c3061f4d78511a8b0b62ccd732f54a7
vann261 <vann31899@gmail.com>
2024-11-01 21:31:46 +0700

    tin tuc 6

--- KoiProjectMain/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
+++ KoiProjectMain/KoiProject.Service/obj/Debug/net8.0/KoiProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bad7081ae704d6cb38c0e539a3ae70edaf052d65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c6aebaecdbb84ac17aaf5a9c745597e9f9a329")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
