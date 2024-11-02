2b4e43a104ddec7c02a7143ac9c3d25e9c0cb3d5
unknown <phanduongkhang123@gmail.com>
2024-11-03 00:17:49 +0700

    update

--- KoiProjectMain/KoiProject.Repositories/obj/Debug/net8.0/KoiProject.Repositories.AssemblyInfo.cs
+++ KoiProjectMain/KoiProject.Repositories/obj/Debug/net8.0/KoiProject.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92820d7864e37668e29fb72f62066733fdf81de7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d7b3d3efe37ae1f72cdba5ab8256e1a5717cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
