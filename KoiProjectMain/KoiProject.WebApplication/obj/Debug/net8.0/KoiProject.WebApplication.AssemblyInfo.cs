f5229d2573d0dd96a39003166bf3dcc5f32bb9da
phu le <lephu240905@gmail.com>
2024-11-10 17:23:10 +0700

    update close for TuVan

--- KoiProjectMain/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
+++ KoiProjectMain/KoiProject.WebApplication/obj/Debug/net8.0/KoiProject.WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b19e128966130aeb52372ac6ab365ff3cb393a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8bbd90cab0c1d847623542c41e591b39aa80cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiProject.WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
